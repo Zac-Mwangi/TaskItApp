e1e83e8202a758281d9a778d702f24d1f02f13a7
zack-mwangi <zackmwangi998@gmail.com>
2021-07-07 09:31:54 +0300

    some few changes

--- app/src/main/java/com/example/taskit/Extras/savedInfo.java
+++ app/src/main/java/com/example/taskit/Extras/savedInfo.java
@@ -2,5 +2,5 @@ package com.example.taskit.Extras;
 
 public class savedInfo {
     //public static String theUrl="http://192.168.100.4/taskit/php/";
-    public static String theUrl="http://192.168.137.1/taskit/php/";
+    public static String theUrl="http://192.168.137.1/TaskItApp/php/";
 }
